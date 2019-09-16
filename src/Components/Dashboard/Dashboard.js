8663a627cac9ac9b725ab3db29cb00417f542247
Markosaur <mark3pace@gmail.com>
2019-09-16 09:07:07 -0600

    working on delete

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 import Product from '../Product/Product'
+import Axios from 'axios'
 
 export default class Dashboard extends Component {
 
-
+    handleDeleteProduct(id) {
+        Axios.delete(`http://localhost:4000/api/inventory/${id}`)
+        .then(res=>{
+            this.props.getInventory(res.data)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
     render() {
 
         const mappedInventory = this.props.inventory.map((product, i)=>{
-            return(<Product key = {i} product = {product} />)
+            return(<Product key = {i} product = {product} delete = {this.handleDeleteProduct}/>)
         })
         
-        
+      
         return (
             <div>
                 {mappedInventory}
+                
             </div>
         )
     }
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 
 export default class Product extends Component {
     render() {
-        let {name, price, img} = this.props.product
+        let {id, name, price, img} = this.props.product
         return (
             <div>
                 <h3>{name}</h3>
                 <h3>{price}</h3>
                 <img src={img} alt="product" />
+                <button onClick={this.props.handleDeleteProduct(id)}>Delete</button>
             </div>
         )
     }
